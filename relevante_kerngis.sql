1abb2e8db64f375344a2c507365d6f824e949ae1
Arno <arno.timmer@geodan.nl>
2020-03-16 16:40:38 +0100

    relevant data from kerngis

--- relevante_kerngis.sql
+++ relevante_kerngis.sql
@@ -1,17 +1,17 @@
-
-select * from bomen.xy_bomen
+drop table if exists kerngis.relevante_vlakken;
 create table kerngis.relevante_vlakken as(
-select objectteks from kerngis.groen_vlakken where objectteks in ('bomengroep', 'Bomen', 'Boomgroep', 'Bomengroep',
-																		   'bmn en Struiken', 'Loofbos', 'Bos (vervalt)',
-																		   'Bomen en struiken', 'Bmn en Struiken', 'Bos', 'bmn',
+select * from kerngis.groen_vlakken where objectteks in ('bomengroep', 'Bomen', 'Boomgroep', 'Bomengroep', 
+																		   'bmn en Struiken', 'Loofbos', 'Bos (vervalt)', 
+																		   'Bomen en struiken', 'Bmn en Struiken', 'Bos', 'bmn', 
 																		   'bomen en Struiken', 'Boomn groep', 'bos', 'bmn en strkn')
 );
 
+drop table if exists kerngis.relevante_punten;
 create table kerngis.relevante_punten as (
 select * from kerngis.groen_punten where objectteks in ('Boom', 'Solitaire boom', 'BooM')
 );
 
+drop table if exists kerngis.relevante_lijnen;
 create table kerngis.relevante_lijnen as (
 select * from kerngis.groen_lijnen where soort in ('BR')
 );
-
